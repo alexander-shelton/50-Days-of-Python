b00a6c686a6e83846ada0abf00778eed2b1c943b
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2025-08-14 23:06:44 -0400

    Add Day 23 solution and tests.

--- three/day_23.py
+++ three/day_23.py
@@ -2,22 +2,55 @@
 #
 # Day 23: Simple Calculator
 
-class Calculator:
-    def __init__(self):
-        pass
-
-    def add(self):
-        pass
+def calculator():
+    operations = {
+        "a": {"name": "ADD", "symbol": "+"}, 
+        "s": {"name": "SUBTRACT", "symbol": "-"}, 
+        "d": {"name": "DIVIDE", "symbol": "/"}, 
+        "m": {"name": "MULTIPLY", "symbol": "*"}
+    }
+    while True:
+        operation = input("Select an operation: [ a | s | d | m ]: ").lower()
+        if operation in ('a', 's', 'd', 'm'):
+            ask = input(f"You selected {operations.get(operation)['name']}, is this correct? [Y/n]: ")
+            if ask == "" or ask in ("Y", "y"):
+                operation = operations.get(operation)['symbol']
+                break
+        print("Enter a valid operation. (e.g., a | s | d | m)")
     
-    def subtract(self):
-        pass
+    while True:
+        try:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            break
+        except ValueError:
+            print("Enter a valid number.")
 
-    def divide(self):
-        pass
-
-    def multiply(self):
-        pass
-
+    if operation == '+':
+        result = num1 + num2
+        return f"{num1} {operation} {num2} = {result}"
+    elif operation == '-':
+        result = num1 - num2
+        return f"{num1} {operation} {num2} = {result}"
+    elif operation == '/':
+        try:
+            result = num1 / num2
+            return f"{num1} {operation} {num2} = {int(result)}"
+        except ZeroDivisionError:
+            return "Sorry you can not divide a number by 0"
+    elif operation == '*':
+        result = num1 * num2
+        return f"{num1} {operation} {num2} = {result}"
+        
 ## Extra Challenge: Multiply Words
+from string import ascii_uppercase
+
 def multiply_words(string: str) -> str:
-    pass
+    words = string.split(' ')
+    total = 1
+    string_words = []
+    for word in words:
+        if word.lower() == word:
+            total *= len(word)
+            string_words.append(f"{word} ({len(word)})")
+    return f"{total}: {', '.join(string_words)}" 
--- three/tests/test_day_23.py
+++ three/tests/test_day_23.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python3
 import unittest
-from day_23 import Calculator, multiply_words
+from unittest.mock import patch
+from day_23 import calculator, multiply_words
 
 class TestDay23(unittest.TestCase):
 
     # Day 23: Simple Calculator
     def test_add(self):
-        #self.calculator = Calculator
-        num1 = 1
-        num2 = 4
-        expected = 5
-        #self.assertEqual(self.calculator.add(num1, num1), expected)
+        with patch("builtins.input", side_effect=['a', 'y', 100, 100]):
+            result = calculator()
+            expected = "100 + 100 = 200"
+            self.assertEqual(result, expected)
 
     def test_subtract(self):
-        num1 = 4
-        num2 = 1
-        expected = 3
-        #self.assertEqual(self.calculator.subtract(num1, num1), expected)
+        with patch("builtins.input", side_effect=['s', 'y', 100, 100]):
+            result = calculator()
+            expected = "100 - 100 = 0"
+            self.assertEqual(result, expected)
         
     def test_divide(self):
-        num1 = 10
-        num2 = 2
-        expected = 5
-        #self.assertEqual(self.calculator.divide(num1, num1), expected)
+        with patch("builtins.input", side_effect=['d', 'y', 100, 100]):
+            result = calculator()
+            expected = "100 / 100 = 1"
+            self.assertEqual(result, expected)
+    
+    def test_zerodivision_error(self):
+        with patch("builtins.input", side_effect=['d', 'y', 100, 0]):
+            result = calculator()
+            expected = "Sorry you can not divide a number by 0"
+            self.assertEqual(result, expected)
 
     def test_multiply(self):
-        num1 = 5
-        num2 = 1
-        expected = 5
-        #self.assertEqual(self.calculator.add(num1, num1), expected)
+        with patch("builtins.input", side_effect=['m', 'y', 100, 100]):
+            result = calculator()
+            expected = "100 * 100 = 10000"
+            self.assertEqual(result, expected)
 
     def test_multiply_words(self):
         string = "love live and laugh"
         expected = "240: love (4), live (4), and (3), laugh (5)"
-        #self.assertEqual(multiply_words(string), expected)
+        self.assertEqual(multiply_words(string), expected)
         string = "Hate war love Peace"
-        expected = "12 – war ( 3) love ( 4)"
-        #self.assertEqual(multiply_words(string), expected)
+        expected = "12: war (3), love (4)"
+        self.assertEqual(multiply_words(string), expected)
 
 if __name__ == '__main__':
     unittest.main()
