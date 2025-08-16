fe37e4255bd2b399d3b3c82a47de4ade8aa8933b
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2025-08-16 03:31:19 -0400

    Add Day 25 solutions and tests.

--- three/tests/test_day_25.py
+++ three/tests/test_day_25.py
@@ -7,13 +7,17 @@ class TestDay25(unittest.TestCase):
     # Day 25: All the Same  
     def test_all_the_same(self):
         arg = ["Mary", "Mary", "Mary"]
-        #self.assertTrue(all_the_same(arg))
+        self.assertTrue(all_the_same(arg))
+        arg = 'bbbbbbbbbb'
+        self.assertTrue(all_the_same(arg))
+        arg = ('a', 'a', 'a', 'a')
+        self.assertTrue(all_the_same(arg))
         
     ## Extra Challenge:  Reverse a String
     def test_read_backwards(self):
         str1 = "the love is real"
-        expected = "real is love the."
-        #self.assertEqual(read_backwards(str1), expected)
+        expected = "real is love the"
+        self.assertEqual(read_backwards(str1), expected)
 
 if __name__ == '__main__':
     unittest.main()
