f6b43b95fa4fd33af11a5ea8f6bb17f0406c4b85
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2025-08-19 08:50:56 -0400

    Add Day 37 solution and tests.

---
+++ four/tests/test_day_37.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import unittest
+from day_37 import count_the_vowels
+
+class TestDay22(unittest.TestCase):
+
+    # Day 37: Count the Vowels
+    def test_count_the_vowels1(self):
+        string = 50
+        expected = "invalid input"
+        self.assertEqual(count_the_vowels(string), expected)
+    
+    def test_count_the_vowels2(self):
+        string = "hello"
+        expected = 2
+        self.assertEqual(count_the_vowels(string), expected)
+    
+    def test_count_the_vowels3(self):
+        string = "saas"
+        expected = 1
+        self.assertEqual(count_the_vowels(string), expected)
+    
+    def test_count_the_vowels4(self):
+        string = "sly"
+        expected = "The string has no vowels."
+        self.assertEqual(count_the_vowels(string), expected)
+
+if __name__ == '__main__':
+    unittest.main()
