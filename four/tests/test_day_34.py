6ebc9bbe1d27cca5dc7de9663639cfd31ac93c50
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2025-08-19 10:58:30 -0400

    Fix comments on day 34 tests.

--- four/tests/test_day_34.py
+++ four/tests/test_day_34.py
@@ -5,7 +5,7 @@ from day_34 import just_digits
 
 class TestDay34(unittest.TestCase):
 
-    # Day 21: List of Tuples
+    # Day 34: Just Digits  
     def test_just_digits(self):
         expected = "1991, 2, 2000, 3, 2008"
         self.assertEqual(just_digits('./python.csv'), expected)
